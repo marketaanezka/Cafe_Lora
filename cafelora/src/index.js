8f36b9da912954b9049b2760a4b77c8d75d55bad
Marketa Krucka <marketakrucka@gmail.com>
2021-04-28 22:15:44 +0200

    finished cafe lora

--- cafelora/src/index.js
+++ cafelora/src/index.js
@@ -16,43 +16,6 @@ navItems.forEach((navItem) => {
   });
 });
 
-const drinks = [
-  {
-    id: 'cappuccino',
-    name: 'Cappuccino',
-    ordered: false,
-    layers: [
-      {
-        color: '#feeeca',
-        label: 'mléčná pěna',
-      },
-      {
-        color: '#fed7b0',
-        label: 'teplé mléko',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-  },
-  {
-    id: 'romano',
-    name: 'Romano',
-    ordered: false,
-    layers: [
-      {
-        color: '#fbdf5b',
-        label: 'citrón',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-  },
-];
-
 const drinksList = document.querySelector('.drinks-list');
 // drinks.forEach((drink) => {
 //   drinksList.appendChild(Drink(drink))
